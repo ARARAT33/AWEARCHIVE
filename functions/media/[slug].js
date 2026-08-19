3900b04885cb8f6dc46efe5b2a0ebda2cd167ba6
Ararat <araratavetisyan777999@gmail.com>
2026-08-20 01:01:13 +0400

    refactor: make public media fully static

--- functions/media/[slug].js
+++
@@ -1,2 +0,0 @@
-import {getItem,proxySource} from '../_shared/archive.js';
-export async function onRequest(context){const item=await getItem(context.request,context.params.slug);if(!item||!['image','video'].includes(item.type||item.item_type))return new Response('Not found',{status:404});return proxySource(item,context.request,'inline')}
